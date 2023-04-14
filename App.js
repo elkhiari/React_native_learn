666f3024762cc07a23a479774ca337d51e3229e7
Othmane Elkhiari <oelkhiar@e1r1p1.1337.ma>
2023-04-14 17:32:40 +0000

    task app

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import axios from 'axios'
+import Task from './components/Tasks';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionText}>Today's tasks</Text>
+        <View style={styles.items}>
+          <Task text={'task n 1'} />
+          <Task text={'task n 2'} />
+        </View>
+      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "IOS" ? 'padding' : 'height'}
+        style={styles.WriteTask}
+        >
+          <TextInput style={styles.input} placeholder='Write a task'/>
+
+          <TouchableOpacity>
+            <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
+            </View>
+
+          </TouchableOpacity>
+
+        </KeyboardAvoidingView>
     </View>
   );
 }
@@ -13,8 +33,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper:{
+    paddingTop:80,
+    paddingHorizontal:20,
+  },
+  sectionText:{
+    fontSize:24,
+    fontWeight:'bold',
+  },
+  items:{
+    marginTop:25,
+  },
+  WriteTask:{
+    position:'absolute',
+    bottom:20,
+    width:'100%',
+    flexDirection:'row',
+    justifyContent:'space-around',
+    alignItems:'center',
+  },
+  input:{
+    paddingVertical:15,
+    paddingHorizontal:15,
+    width:250,
+    backgroundColor:'#FFF',
+    borderColor:'#C0C0C0',
+    borderWidth:1,
+    width:250,
+
+  },
+  addWrapper:{
+    width:60,
+    height:60,
+    backgroundColor:'#FFF',
+    justifyContent:'center',
+    alignItems:'center',
+    borderColor:'#C0C0C0',
+    borderWidth:1,
+  },
+  addText:{},
 });
